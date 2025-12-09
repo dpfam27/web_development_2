f9eee888e14df467c51710eb1058e4b950f6f59c
nghuyenthuong <nguyenhuyenthuong2707@gmail.com>
2025-12-09 12:58:52 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,6 +3,6 @@
 session_start(); 
 
 //creating a database connection - $link is a variable use for just connection class
-$link=mysqli_connect("localhost","root","Klyvu1712@") or die(mysqli_connect_error());
+$link=mysqli_connect("localhost","root","") or die(mysqli_connect_error());
 mysqli_select_db($link,"product_manage") or die(mysqli_error($link));
 ?>
